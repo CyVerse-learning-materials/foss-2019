a1d0608e2c976ea9167cfa91b56c466f0054104f
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2019-05-27 11:03:45 -0700

    Create version_control.rs

---
+++ software_essentials/version_control.rs
@@ -0,0 +1,90 @@
+.. include:: cyverse_rst_defined_substitutions.txt
+
+|CyVerse logo|_
+
+|Home_Icon|_
+`Learning Center Home <http://learning.cyverse.org/>`_
+
+
+Version Control 
+---------------
+
+`Version Control <https://en.wikipedia.org/wiki/Version_control>`_ is an important component of software development which manage changes in documents, websites, code, or other digital information.
+
+The `most commmon version control software <https://en.wikipedia.org/wiki/List_of_version-control_software>`_ in data science are `git`, `svn`, `cvs`, and `bzr`.
+
+Version control enable reproducibility in data science.
+
+.. #### Comment: short description
+
+**Some things to remember about these platforms**
+
+- For the sake of time, we are only going to cover `Git <https://git-scm.com/`_ and one web-based hosting service (`Github <https://github.com/>`_).
+- Determine what your community is using, and learn that first. 
+- The most popular software for data science applications are `git` and `svn` and are integrated with IDE like `RStudio <https://support.rstudio.com/hc/en-us/articles/200532077-Version-Control-with-Git-and-SVN>`_ and `Jupyter Lab <https://github.com/jupyterlab/jupyterlab-git>`_
+
+
+----
+
+*Git ready*
+~~~~~~~~~~~
+
+  1. Install Git
+      - `Website <https://git-scm.com/downloads>`_
+
+  2. Create a Github Account
+      - Website <https://github.com/join?source=header-home>`_
+      - **Important: If you are an academic and have a `*.edu` email address you can get Educational Account benefits.** 
+..
+	#### Comment: Suggested style guide:
+	1. Steps begin with a verb or preposition: Click on... OR Under the "Results Menu"
+	2. Locations of files listed parenthetically, separated by carets, ultimate object in bold
+	(Username > analyses > *output*)
+	3. Buttons and/or keywords in bold: Click on **Apps** OR select **Arabidopsis**
+	4. Primary menu titles in double quotes: Under "Input" choose...
+	5. Secondary menu titles or headers in single quotes: For the 'Select Input' option choose...
+	####
+
+
+----
+
+**Fix or improve this documentation:**
+
+- On Github: |Github Repo Link|
+- Send feedback: `Tutorials@CyVerse.org <Tutorials@CyVerse.org>`_
+- Live chat/help: Click on the |intercom| on the bottom-right of the page for questions on documentation
+
+----
+
+
+.. Comment: Place Images Below This Line
+   use :width: to give a desired width for your image
+   use :height: to give a desired height for your image
+   replace the image name/location and URL if hyperlinked
+
+
+ .. |Clickable hyperlinked image| image:: ./img/IMAGENAME.png
+    :width: 500
+    :height: 100
+ .. _CyVerse logo: http://learning.cyverse.org/
+
+ .. |Static image| image:: ./img/IMAGENAME.png
+    :width: 25
+    :height: 25
+
+
+
+.. Comment: Place URLS Below This Line
+
+   # Use this example to ensure that links open in new tabs, avoiding
+   # forcing users to leave the document, and making it easy to update links
+   # In a single place in this document
+
+   .. |Substitution| raw:: html # Place this anywhere in the text you want a hyperlink
+
+      <a href="REPLACE_THIS_WITH_URL" target="blank">Replace_with_text</a>
+
+
+.. |Github Repo Link|  raw:: html
+
+   <a href="https://github.com/CyVerse-learning-materials/foss-2019/tree/master" target="blank">Github Repo Link</a>
