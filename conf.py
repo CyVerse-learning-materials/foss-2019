07d0ffaa7fd58e7511bd1f360417876b138e60e3
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-18 10:45:35 -0400

    add triage issue template

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Version 2.0, May 2020
 # -*- coding: utf-8 -*-
 #
 # General information about the project.
@@ -9,13 +10,13 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'CyVerse Learning Institute Foundational Open Science Skills 2019'
-copyright = '2019, CyVerse'
-author = 'CyVerse'
-version = '0.1.0'
-release = '0.1.0'
+project = 'YOUR GUIDE NAME HERE'
+copyright = '2020, CyVerse'
+author = 'your_name'
+version = '2.0'
+release = '2.0'
 
-epub_title = 'CyVerse Learning Institute Foundations of Open Science Skills 2019'
-epub_author = 'Ramona L. Walls, Tyson L. Swetnam, Amanda Cooksey, Meghan Balk'
-epub_publisher = 'CyVerse'
-epub_copyright = 'CC0 - Open Source'
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
